9e7c282378caec6c7c7c5edd3b5f3ccadbc58515
Sawyer <sawyer@SawyerdeMacBook-Pro.local>
2021-03-09 10:14:10 +0800

    Add username in the build image

--- android_build/buildDocker/build_docker.sh
+++ android_build/buildDocker/build_docker.sh
@@ -1,1 +1,1 @@
-docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build .
+docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t android-build_$(id -un) .
